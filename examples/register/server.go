fad7b3ef2de0d92b0cc1bae670de31d963915a24
reid <reid@scogg.in>
2023-09-27 15:11:33 -0500

    dont broadcast docker ips

--- examples/register/server.go
+++ examples/register/server.go
@@ -9,7 +9,7 @@ import (
 
 	"time"
 
-	"github.com/grandcat/zeroconf"
+	"github.com/Native-Planet/zeroconf"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/grandcat/zeroconf
+module github.com/Native-Planet/zeroconf
 
 go 1.13
 
